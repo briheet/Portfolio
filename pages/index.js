7239ef4251dfd3601cf40c99cec52061b9e46bb3
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-07-04 08:15:10 +0530

    idk

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from "@chakra-ui/react"
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg">
+        Hello, I&apos;m a full-stack developer based in delhi, India!
+      </Box>
+      <div>Hello</div>
+    </Container>
+  )
+}
+
+export default Page
